52a5ac5991b00fe00b21abefe32dd0d13f1ac988
Endika7593 <e.acedo93@gmail.com>
2024-05-09 15:05:42 +0200

    editando perfiles

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '../images/avatar15.jpg'
+import { FaEdit, FaCheck } from 'react-icons/fa'
 
 const UserProfile = () => {
+  const [avatar, setAvatar] = useState('')
   return (
-    <div>
-      USER PROFILE
-    </div>
+    <section className="profile">
+      <div className="container profile__container">
+        <Link to={`/myposts/sdfsdf`} className="btn">My Posts</Link>
+
+        <div className="profile__details">
+          <div className="avatar__wrapper">
+            <div className="profile__avatar">
+              <img src={Avatar} alt="" />
+            </div>
+            {/* Formulairo para actuaizar el avatar */}
+            <form className="avatar__form">
+              <input type="file" name="avatar" id="avatar" onCange={e => setAvatar(e.target.files[0])} accept="png, jpg, jpeg" />
+              <label htmlFor="avatar"><FaEdit/></label>
+            </form>
+            <button className='profile__avatar-btn'><FaCheck/></button>
+          </div>
+
+          <h1>Ernest Achiever</h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
