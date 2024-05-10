cb2aa0157bdb260d0c6524a438b6e53260103f85
Endika7593 <e.acedo93@gmail.com>
2024-05-10 13:33:12 +0200

    hechos los middlewares, los usercontrollers, cambiar el avatar, confirmar cambio de datos, contraseñas, emails, etc... todo probado en postman y  FUNCIONA

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,8 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
+const path = require('path')
+const {v4: uuid} = require("uuid")
 
 const User = require('../models/userModel')
 
@@ -51,7 +55,30 @@ const registerUser = async (req, res, next) => {
 // POST : api/users/login
 //UNPROTECTED
 const loginUser = async (req, res, next) => {
-    res.json("Login user")
+    try {
+        const {email, password} = req.body;
+        if(!email || !password) {
+            return next(new HttpError("Fill in all fields", 422))
+        }
+        const newEmail = email.toLowerCase();
+
+        const user = await User.findOne({email: newEmail})
+        if(!user) {
+            return next(new HttpError("Invalid credentials", 422))
+        }
+
+        const comparePass = await bcrypt.compare(password, user.password)
+        if(!comparePass) {
+            return next(new HttpError("Invalid credentials", 422))
+        }
+
+        const {_id: id, name} = user;
+        const token = jwt.sign({id, name}, process.env.JWT_SECRET, {expiresIn: '1d'})
+
+        res.status(200).json({token, id, name})
+    } catch (error) {
+        return next(new HttpError("Login failed. Please check your credentials", 422))
+    }
 }
 
 
@@ -64,7 +91,16 @@ const loginUser = async (req, res, next) => {
 // POST : api/users/:id
 //PROTECTED
 const getUser = async (req, res, next) => {
-    res.json("User profile")
+    try {
+        const {id} = req.params;
+        const user = await User.findById(id).select('-password');
+        if(!user) {
+            return next(new HttpError("User not found", 404))
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
@@ -78,7 +114,46 @@ const getUser = async (req, res, next) => {
 // POST : api/users/change-avatar
 //PROTECTED
 const changeAvatar = async (req, res, next) => {
-    res.json("Change avatar")
+    try {
+        if(!req.files.avatar) {
+            return nexxt(new HttpError("Please chose an image", 422))
+        }
+
+        // fnid user from database
+        const user = await User.findById(req.user.id)
+        // delete old avatar if it exists
+        if(user.avatar) {
+            fs.unlinkSync(path.join(__dirname,  '..', 'uploads', user.avatar), (err) => {
+                if(err) {
+                    return next(new HttpError("Error deleting old avatar", 422))
+                }
+            })
+        }
+
+        const {avatar} = req.files;
+        // check file size
+        if(avatar.size > 50000) {
+            return next(new HttpError("Image size too large. It must be less than 500kb", 422))
+        }
+
+        let fileName;
+        fileName = avatar.name;
+        let splittedFilename = fileName.split('.')
+        let newFilename = splittedFilename[0] + uuid() + '.' + splittedFilename[splittedFilename.length - 1]
+        avatar.mv(path.join(__dirname, '..', 'uploads', newFilename), async (err) => {
+            if(err) {
+                return next(new HttpError(err))
+            }
+
+            const updatedAvatar = await User.findByIdAndUpdate(req.user.id, {avatar: newFilename}, {new: true})
+            if(!updatedAvatar) {
+                return next(new HttpError("Avatar couldn't be changed", 422))
+            }
+            res.status(200).json(updatedAvatar)
+        })
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
@@ -92,7 +167,52 @@ const changeAvatar = async (req, res, next) => {
 // POST : api/users/edit-user
 //PROTECTED
 const editUser = async (req, res, next) => {
-    res.json("Edit user details")
+    try {
+        const {name, email, currentPassword, newPassword, confirmNewPassword} = req.body;
+        if(!name || !email || !currentPassword || !newPassword) {
+            return next(new HttpError("Fill in all fields", 422))
+        }
+
+        // get user from database
+        const user = await User.findById(req.user.id);
+        if(!user) {
+            return next(new HttpError("User not found", 404))
+        }
+
+        //make sure new email doesn't already exist
+
+        const emailExist = await User.findOne({email});
+
+
+        //if we want to update other details with or without changing the email (which is the only id as we use it to login)
+
+        if(emailExist && (emailExist._id != req.user.id)) {
+            return next(new HttpError("Email already exists", 422))
+        }
+        // compare current password to database password
+
+        const validateUserPassword = await bcrypt.compare(currentPassword, user.password);
+        if(!validateUserPassword) {
+            return next(new HttpError("Invalid current password", 422))
+        }
+
+        // compare new passwqords
+        
+        if(newPassword !== confirmNewPassword) {
+            return next(new HttpError("New passwords do not match", 422)) 
+        }
+
+        // hash new password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(newPassword, salt) ;
+
+        // update user details in database
+        const newInfo = await User.findByIdAndUpdate(req.user.id, {name, email, password: hash}, {new: true})
+        res.status(200).json(newInfo)
+
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
@@ -102,11 +222,16 @@ const editUser = async (req, res, next) => {
 
 
 
-// CHANGE USER AVATAR GET AUTHORS 
+//  GET AUTHORS 
 // POST : api/users/authors
 //UNPROTECTED
 const getAuthors = async (req, res, next) => {
-    res.json("Get all users/authors")
+    try {
+        const authors = await User.find().select('-password');
+        res.json(authors);
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
